236c68c94f779b370e6f1a2e887e9064b17a8a2c
Nasty <anfirunovich@gmail.com>
2021-12-28 15:27:42 +0300

    add Serializers

--- supplier/serializers.py
+++ supplier/serializers.py
@@ -3,13 +3,12 @@ from rest_framework import serializers
 from supplier.models import Supplier
 
 
-class GetSupplierSerializer(serializers.ModelSerializer):
-
+class SupplierSerializer(serializers.ModelSerializer):
     class Meta:
         model = Supplier
         fields = (
-            "name",
-            "description",
-            "balance",
-            "cars",
+            'title',
+            'year_of_foundation',
+            'address',
+            'cars'
         )
