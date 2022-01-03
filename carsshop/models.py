5bae6b6485599424bfdd2c48c17824af5e9891ae
nastechka <anfirunovich@gmail.com>
2022-01-03 22:23:03 +0300

    add class Showroom

--- carsshop/models.py
+++ carsshop/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 
-
 class Car(models.Model):
     model = models.CharField(max_length=256, verbose_name='model')
     color = models.CharField(max_length=256, verbose_name='color')
     year_of_release = models.DateTimeField()
     price = models.DecimalField()
+
+
+class Showroom(models.Model):
+    title = models.CharField(max_length=256, verbose_name='title')
+    description = models.TextField(max_length=256, verbose_name='description')
+    address = models.OneToOneField(null=True, on_delete=models.SET_NULL)
+    year_of_foundation = models.DateTimeField(verbose_name='year_of_foundation')
+
+    customers = models.ManyToManyField('customer.Customer', verbose_name='customers')
+    cars = models.ManyToManyField(Car, verbose_name='cars')
+
+
+
 
 
