996f51a44159de9ceddec7be2e4359bc1e665a0c
nastechka <anfirunovich@gmail.com>
2022-01-28 16:35:18 +0300

    add new class: CarPriceSerializer

--- supplier/serializers.py
+++ supplier/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
 
-from supplier.models import Supplier
+from supplier.models import Supplier, Car
 
 
 class SupplierSerializer(serializers.ModelSerializer):
@@ -12,3 +13,36 @@ class SupplierSerializer(serializers.ModelSerializer):
             'address',
             'cars'
         )
+
+
+class GetSupplierSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Supplier
+        fields = (
+            'title',
+            'year_of_foundation',
+            'address',
+            'cars'
+        )
+
+
+class GetCarSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Car
+        fields = (
+            "model",
+            "year_of_release",
+            "color",
+            "price",
+        )
+
+
+class CarPriceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Car
+        fields = (
+            'model',
+            'price',
+            )
